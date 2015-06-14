9cec7de350d84bb3d4ae9513683b2c0a3d531920
zebimax <zebimax@gmail.com>
2015-06-14 17:49:39 +0300

    functions.php

---
+++ functions.php
@@ -0,0 +1,18 @@
+<?php
+function menu_html()
+{
+    return '<div id="tooplate_menu">
+                <ul>
+                    <li><a href="index.html" class="current">Home</a></li>
+                    <li><a href="about.html">About Us</a></li>
+                    <li><a href="blog.html">Blog</a></li>
+                    <li><a href="gallery.html">Gallery</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                    <li><a href="index_ru.html">Главная</a></li>
+                    <li><a href="contact_ru.html">Конакты</a></li>
+                    <li><a href="gallery_ru.html">Галерея</a></li>
+                    <li><a href="blog_ru.html">Блог</a></li>
+                    <li><a href="about_ru.html">О нас</a></li>
+                </ul>
+            </div> <!-- end of tooplate_menu -->';
+}
